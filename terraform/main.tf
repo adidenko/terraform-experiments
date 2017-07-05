8cedab535b29d888e1ac006a2aa463310ec37026
Oleksandr Didenko <odidenko@odidenko.corpmz.com>
2017-07-05 16:42:47 +0200

    Added provisioner

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,11 +18,39 @@ resource "google_compute_instance" "test" {
   network_interface {
     network = "default"
     access_config {
-      // Ephemeral IP - leaving this block empty will generate a new external IP and assign it to the machine
+      // Ephemeral IP - leaving this block empty will generate a new external
+      // IP and assign it to the machine
     }
   }
 
   metadata {
     ssh-keys = "root:${file("${var.public_key_path}")}"
+  }
+
+  // Provisioning
+  provisioner "file" {
+    source      = "${var.install_script_src_path}"
+    destination = "${var.install_script_dest_path}"
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("${var.private_key_path}")}"
+      agent       = false
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("${var.private_key_path}")}"
+      agent       = false
+    }
+
+    inline = [
+      "chmod +x ${var.install_script_dest_path}",
+      "sudo ${var.install_script_dest_path} ${count.index}",
+    ]
   }
 }
