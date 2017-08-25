f62649c5ae9226baa2be5feb93486d90a83644f5
Oleksandr Didenko <odidenko@mz.com>
2017-08-25 17:40:00 +0200

    Add key workaround for plan CI script

--- scripts/ci_plan.sh
+++ scripts/ci_plan.sh
@@ -3,6 +3,11 @@
 
 set -ex
 
+# For planning we don't need real key, so let's create fake key if real one
+# does not exist
+test -d ~/.ssh || mkdir ~/.ssh
+test -e ~/.ssh/google_compute_engine.pub || touch ~/.ssh/google_compute_engine.pub
+
 pushd $1
 terraform init
 terraform plan
